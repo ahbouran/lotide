9cffdedbd69240575835be3da7ec4d96dd79afda
Ali <alihbouran@gmail.com>
2022-05-02 16:01:49 -0700

    first commit

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  let statement;
+  if (actual === expected) {
+    statement = console.log("👍 Assertion Passed: " + actual + " === " + [expected]);
+  } else if (actual !== expected) {
+    statement = console.log("👎 Assertion Failed: " + actual + " !== " + [expected]);
+  }
+  return statement;
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 2);

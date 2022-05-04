0b2ca4bd351497a908b9c11bfcedfa6ff1531f0e
Ali <alihbouran@gmail.com>
2022-05-03 17:34:11 -0700

    Wrote function comparing two arrays

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  let statement;
+  if (actual === expected) {
+    statement = console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    statement = console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  return statement;
+}; 
+
+const eqArrays = function(arrOne, arrTwo) {
+  let isTheSame = true;
+  for (let i = 0; i < arrOne.length; i++) {
+    //console.log(arrOne[i], arrTwo[i])
+    if (arrOne[i] !== arrTwo[i]) {
+      isTheSame = false
+    } 
+  }
+  return isTheSame;
+}
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), true);
+
